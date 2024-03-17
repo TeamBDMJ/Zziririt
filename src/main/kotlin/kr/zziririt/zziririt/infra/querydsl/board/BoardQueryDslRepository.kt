acaf42629c04dea4e7df20177af676e0ed7bcb4b
Miso <un5754@naver.com>
2024-03-18 02:00:15 +0900

    ZRR-71 Feat: Board 조회 api 및 정책 적용
    	1. BoardController
    	 - 스트리머 게시판 생성, 수정 신청 폼 api
    	 - 부모 게시판, 자식 게시판 조회 api
    
    	2. BoardEntity
    	 - 게시판 활성/비활성 상태를 나타내는 boardActStatus 추가
    
    	3. BoardService
    	 - 이미지를 제외한 신청 url 주소 및 게시판 이름을 먼저 저장하고 이후에 uploadImage 메서드로 이미지 저장
    	 - boardScheduler 메서드는 매일 자정에 한번 실행됨
    
    	4. MemberController
    	 - 소셜회원이 구독한 게시판 조회 api
    
    	5. MemberService
    	 - getSubscribeByMember 메서드로 구독 게시판 리스트로 반환
    
    	6. OAuth2LoginSuccessHandler
    	 - 토큰 생성 내용 수정
    
    	7. SocialMemberEntity
    	 - 회원별 구독 게시판 리스트 테이블 추가
    
    	8. BoardRepository
    	 - BoardRepositoryImpl 로 분리
    
    	9. BoardQueryDslRepositoryImpl
    	 - findByPageable() : 부모 게시판 조회
    	 - findStreamersByPageable() : 스트리머 게시판 조회
    	 - findBoardStatusToInactive() : 비활동중인 게시판 아이디 리스트 반환
    	 - updateBoardStatusToInactive() : BoardAct.INACTIVE 로 변경시킴
    
    	10. StreamerFormEntity
    	 - 스트리머 폼 엔티티 클래스 작성
    
    	11. StreamerFormDto
    	 - 스트리머 폼 Request
    
    	12. BoardRowDto
    	 - QueryDsl 을 이용할 때 게시판 entity 지정 값만 조회
    
    	13. StreamerBoardRowDto
    	 - QueryDsl 을 이용할 때 스트리머 신청 폼 entity 지정 값만 조회

---
+++ src/main/kotlin/kr/zziririt/zziririt/infra/querydsl/board/BoardQueryDslRepository.kt
@@ -0,0 +1,15 @@
+package kr.zziririt.zziririt.infra.querydsl.board
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+interface BoardQueryDslRepository {
+
+    fun findByPageable(pageable: Pageable): Page<BoardRowDto>
+
+    fun findStreamersByPageable(pageable: Pageable): Page<StreamerBoardRowDto>
+
+    fun findBoardStatusToInactive(): List<Long>
+
+    fun updateBoardStatusToInactive(inactiveBoardIdList: List<Long>)
+}
