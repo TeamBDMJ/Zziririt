f36ff6bcf98b6cdc34b3f61c41a7b0665b2b7adc
doojoo9999 <parkbr95@naver.com>
2024-03-28 22:54:19 +0900

    ZRR-91 Refactor: JavaMailSender 리팩토링
    1. MailService
    - Interface - Service 구조로 재사용성 강화

--- src/main/kotlin/kr/zziririt/zziririt/infra/mailsender/MailService.kt
+++ src/main/kotlin/kr/zziririt/zziririt/infra/mailsender/MailService.kt
@@ -5,12 +5,16 @@ import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Component
 
 @Component
-class MailUtility(
-    val javaMailSender: JavaMailSender
-) {
-    fun sendMailToNewbie(email: String, nickname: String, createdAt: String) {
-        val message = javaMailSender.createMimeMessage()
-        message.addRecipients(Message.RecipientType.TO, email)
+class MailService(
+    private val javaMailSender: JavaMailSender
+) : MailClient {
+
+    val message = javaMailSender.createMimeMessage()
+    val zziririt = "cs@zziririt.kr"
+
+    override fun sendMailToNewbie(recipient: String, nickname: String, createdAt: String) {
+
+        message.addRecipients(Message.RecipientType.TO, recipient)
         message.setSubject("[zziririt] 회원 가입이 완료되었습니다.")
         val msg = """
             <h1>[회원가입 정보]</h1> <br>
@@ -18,7 +22,8 @@ class MailUtility(
             <h2> 가입일: $createdAt
         """.trimIndent()
         message.setText(msg, "UTF-8", "HTML")
-        message.setFrom("cs@zziririt.kr")
+        message.setFrom(zziririt)
         javaMailSender.send(message)
+
     }
 }
